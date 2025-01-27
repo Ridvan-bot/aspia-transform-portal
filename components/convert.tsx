9f78767782f17cc524bae0b1b8c40e3ea4af8a5b
admin <admin@admins-MacBook-Pro.local>
2025-01-27 09:53:48 +0100

    feat: filter out empty rows in Convert component for improved data display

--- components/convert.tsx
+++ components/convert.tsx
@@ -5,19 +5,22 @@ interface ConvertProps {
 }
 
 const Convert: React.FC<ConvertProps> = ({ fileContent }) => {
+  // Filtrera bort tomma rader
+  const filteredContent = fileContent.filter(row => Object.values(row).some(value => value !== ''));
+
   return (
     <div className="container-tabell">
       <div className="flex justify-center pt-4 w-full max-h-screen overflow-auto">
         <table className="table-auto border-collapse border border-gray-400 w-full">
           <thead className="sticky top-0 bg-white">
             <tr>
-              {Object.keys(fileContent[0]).map((key) => (
+              {Object.keys(filteredContent[0]).map((key) => (
                 <th key={key} className="border border-gray-300 px-2 py-2">{key}</th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {fileContent.map((row, rowIndex) => (
+            {filteredContent.map((row, rowIndex) => (
               <tr key={rowIndex}>
                 {Object.values(row).map((value, colIndex) => (
                   <td key={colIndex} className="border border-gray-300 px-2 py-2 break-words">{value as React.ReactNode}</td>
