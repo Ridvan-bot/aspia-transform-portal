1003e3a7dc6b295a1a87bff2e7cd0446f64a29ea
Robin Pohlman <robin.pohlman@gmail.com>
2025-01-29 07:56:02 +0100

    feat: refactor layout structure for improved responsiveness and update footer styling

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,17 +1,14 @@
 import Footer from "@/components/ui/footer";
 
-
 export default function DefaultLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-
-return (
-    <>
-      <main className="relative flex grow flex-col">{children}</main>
-
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">{children}</main>
       <Footer />
-    </>
+    </div>
   );
-};
+}
