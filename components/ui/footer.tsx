5ca2747b204af7b61d7d1dfe0fcdeb2938e8eed0
Robin Pohlman <robin.pohlman@gmail.com>
2025-01-30 07:36:21 +0100

    style: update footer styles for improved visibility and fixed positioning

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <div className="container mx-auto text-center w-full">
+      <div className="container mx-auto text-center w-full ">
         <p>&copy; {new Date().getFullYear()} Aspia</p>
       </div>
     </footer>
