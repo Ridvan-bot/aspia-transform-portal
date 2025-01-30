ef80465569aa13f34a93bcc0ad6da46d1282c2ad
Robin Pohlman <robin.pohlman@gmail.com>
2025-01-30 11:19:52 +0100

    feat: add export functionality and enhance home page tests with header interaction logging

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,10 +1,15 @@
+// Funktion för att mappa index till text
+const indexToText = (index: number): string => {
+  const textMap = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth'];
+  return textMap[index] || `${index + 1}th`;
+};
+
 describe('Home Page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('should have all buttons on the home page', () => {
-    cy.visit('/');
     cy.get('button').contains('Importera CSV').should('exist');
     cy.get('button').contains('Spara Mall').should('exist');
     cy.get('button').contains('Mallar').should('exist');
@@ -30,16 +35,26 @@ describe('Home Page', () => {
     cy.get('table').should('contain', '80,00');
     cy.screenshot();
 
-    // Click on the first header in the table
-    cy.get('table thead tr th').first().click();
-    
-    // Press arrow down and then enter
-    cy.get('body').type('{downarrow}');
-    cy.get('body').type('{enter}');
+    // Log all headers and count them
+    cy.get('table thead tr th').each((header, index) => {
+    }).then((headers) => {
+      const headerCount = headers.length;
+      console.log(`Total headers: ${headerCount}`);
+
+      // Click each header and select the corresponding option from the dropdown
+      headers.each((index, header) => {
+
+        const indexPlusOne = index + 1;
+        cy.get('table thead tr th').eq(index).click();
+        cy.get('table thead tr th').eq(index).find('select').should('be.visible');
+        cy.get('table thead tr th').eq(index).find('select').select(indexPlusOne);
+
+      });
+    });
+
     cy.screenshot();
 
     // Testing Utbetalningsdatum 
-
     cy.get('button').contains('Utbetalningsdatum').click();
     cy.get('.react-datepicker').should('be.visible');
     cy.get('.react-datepicker__day--012').should('be.visible').click();
@@ -54,6 +69,9 @@ describe('Home Page', () => {
     cy.get('input.input-custom[placeholder="Sista dagen i föregående månad"]').should('have.value', '2024-01-29');
     cy.screenshot();
 
-    
+    // Click Exportera button
+    cy.get('button').contains('Exportera').click();
+    cy.screenshot();
   });
+
 });
