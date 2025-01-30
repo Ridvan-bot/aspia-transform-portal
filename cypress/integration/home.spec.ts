0993aa8728163041b1b36d71113325a8bfeda8a0
Robin Pohlman <robin.pohlman@gmail.com>
2025-01-30 07:24:17 +0100

    test: enhance home page tests with additional date input validations and screenshot captures

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -28,6 +28,15 @@ describe('Home Page', () => {
     cy.get('table').should('contain', '2023-01-18');
     cy.get('table').should('contain', '705');
     cy.get('table').should('contain', '80,00');
+    cy.screenshot();
+
+    // Click on the first header in the table
+    cy.get('table thead tr th').first().click();
+    
+    // Press arrow down and then enter
+    cy.get('body').type('{downarrow}');
+    cy.get('body').type('{enter}');
+    cy.screenshot();
 
     // Testing Utbetalningsdatum 
 
@@ -35,6 +44,16 @@ describe('Home Page', () => {
     cy.get('.react-datepicker').should('be.visible');
     cy.get('.react-datepicker__day--012').should('be.visible').click();
     cy.screenshot();
+
+    // Testing Första dagen i föregående månad
+    cy.get('input.input-custom[placeholder="Första dagen i föregående månad"]').clear().type('2024-01-05');
+    cy.get('input.input-custom[placeholder="Första dagen i föregående månad"]').should('have.value', '2024-01-05');
+
+    // Testing Sista dagen i föregående månad
+    cy.get('input.input-custom[placeholder="Sista dagen i föregående månad"]').clear().type('2024-01-29');
+    cy.get('input.input-custom[placeholder="Sista dagen i föregående månad"]').should('have.value', '2024-01-29');
+    cy.screenshot();
+
     
   });
 });
